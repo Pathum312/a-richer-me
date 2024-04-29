5c17f038a9f6cce084b5ca9c587d129a8f866a13
Pathum312 <pathum.2019557@iit.ac.lk>
2024-04-30 00:32:09 +0530

    Small changes

--- components/Table.js
+++ components/Table.js
@@ -3,14 +3,30 @@
 import { useState } from 'react';
 import Calendar from './Calendar';
 import styles from './table.module.css';
+import Image from 'next/image';
 
 const Table = () => {
 	const columns = ['Date', 'Amount', 'Description', 'Category'];
-	const data = [
-		{ date: '01/01/2024', amount: '$79.99', description: 'Uber', category: 'something' },
-		{ date: '01/01/2024', amount: '$79.99', description: 'Uber', category: 'something' },
-		{ date: '01/01/2024', amount: '$79.99', description: 'Uber', category: 'something' },
-	];
+	const categories = {
+		Travel: 'pink',
+		Food: 'green',
+		Personal: 'red',
+		Health: 'blue',
+		Home: 'light_pink',
+		Pets: 'light_blue',
+		Gifts: 'sea_green',
+		Utilities: 'orange',
+	};
+
+	// Table data
+	const [data, setData] = useState([
+		{ date: '01/01/2024', amount: '$79.99', description: 'Uber', category: 'Travel' },
+		{ date: '01/01/2024', amount: '$79.99', description: 'Uber', category: 'Food' },
+		{ date: '01/01/2024', amount: '$79.99', description: 'Uber', category: 'Personal' },
+		{ date: '01/01/2024', amount: '$79.99', description: 'Uber', category: 'Home' },
+	]);
+
+	// Filter date
 	const [selectedDate, setSelectedDate] = useState(new Date());
 
 	// This month is used while getting the finance data.
@@ -40,7 +56,12 @@ const Table = () => {
 					{data.map((row, index) => (
 						<tr key={index} className={styles.row}>
 							{Object.values(row).map((value, colIndex) => (
-								<td key={colIndex} className={styles.column}>
+								<td
+									key={colIndex}
+									className={`${styles.column} ${styles.column_data} ${
+										colIndex === 3 && categories[value] ? styles[categories[value]] : ''
+									}`}
+								>
 									{value}
 								</td>
 							))}
