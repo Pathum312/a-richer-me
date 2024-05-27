72cfdf49613190bcd0c8097aa75af7101c628e66
Pathum312 <pathum.2019557@iit.ac.lk>
2024-05-27 12:15:44 +0530

    Fixed layout redundancy

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,17 @@
 import './globals.css';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 import StoreProvider from './StoreProvider';
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body>
-				<StoreProvider>{children}</StoreProvider>
+				<StoreProvider>
+					<Header />
+					{children}
+					<Footer />
+				</StoreProvider>
 			</body>
 		</html>
 	);
